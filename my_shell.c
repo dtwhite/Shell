442bab6d31091f6c3075e08cfe94b5211be737b7
Daniel White <dwhite8830@gmail.com>
2020-02-16 10:28:46 -0500

    Testing

--- my_shell.c
+++ my_shell.c
@@ -85,7 +85,7 @@ int main(int argc, char* argv[]) {
 			int retval = fork();
 			if(retval == 0){
 				execvp(tokens[0], tokens);
-				perror("Shell: Incorrect Command");
+				printf("Shell: Incorrect Command");
 				exit(1);
 			}
 			else{
