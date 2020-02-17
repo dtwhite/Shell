2ed691210bc88a71e1688f196cdcfc586f968e24
Daniel White <dwhite8830@gmail.com>
2020-02-16 22:48:56 -0500

    Can run in the background now

--- my_shell.c
+++ my_shell.c
@@ -95,7 +95,7 @@ int main(int argc, char* argv[]) {
 			continue;
 		}
 		int basePointer = 0;
-		while(tokens[basePointer] != NULL){
+		while(*tokens[basePointer] != NULL){
 			int futurePointer = grabCommand(tokens, basePointer);
 			for(i=0;tokens[i]!=NULL;i++){
 				printf("found token %s (remove this debug output later)\n", tokens[i]);
