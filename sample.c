8b4ca549c6572c01c05561b4b1f02fc01cbfa3ca
Daniel T  White <dtwhite0@th121-19.cs.wm.edu>
2020-02-18 13:56:24 -0500

    Updated the sample file

--- sample.c
+++ sample.c
@@ -1,37 +1,68 @@
-int retval = fork();
-if(isParallelCommand(command)){
-	char delimiter[] = "&&&";
-	int indexPointer = 0;
-	while(command[indexPointer] != NULL){
-        int newIndex = grabCommand(command, indexPointer, &delimiter);
-		char **copyCommand = copyTokens(command, index, newIndex);
-		if(command[newIndex] != NULL && strcmp(command[newIndex], delimiter) == 0)
-			newIndex++;
-		executeShellBuiltin(copyCommand);
-		indexPointer = newIndex;
-		if()
-	}
+void reapDeadChildren(){
+	
 }
-else if(isBackgroundCommand(command)){
-	background = true;
-    int retval = fork()
-    if(retval == 0)
-	    executeShellBuiltin(command);
-    else{
-        
-    }
-}
-else{
-    int retval = fork();
-    if(retval == 0)
-	    executeShellBuiltin(command);
-    else{
-		int status;
-		int pid = retval;
-		if(!background || !lastParallel)
-			waitpid(pid, &status, 0);
+
+int basePointer = 0;
+while(tokens[basePointer] != NULL){
+	char delim[]= "&&";
+	int futurePointer = grabCommand(tokens, basePointer, &delim);
+	char **command = copyTokens(tokens, basePointer, futurePointer);
+	if(isParallelCommand(command)){
+		char delimiter[] = "&&&";
+		int indexPointer = 0;
+		bool lastCommand = false;
+		while(command[indexPointer] != NULL){
+	        int newIndex = grabCommand(command, indexPointer, &delimiter);
+			char **copyCommand = copyTokens(command, index, newIndex);
+			if(command[newIndex] != NULL && strcmp(command[newIndex], delimiter) == 0)
+				newIndex++;
+			if(strcmp(command[newIndex], "cd") == 0){ // This checks if the given command is cd and makes the chdir system call.
+				int result = chdir(tokens[newIndex+1]);
+				if(result == -1)
+					perror("Shell");
+			}
+			else{
+				if(command[newIndex] == NULL)
+					lastCommand = true;
+				int retval = fork();
+				if(retval == 0){
+					executeShellBuiltin(copyCommand);
+				}
+				else{
+					if(lastCommand){
+						int pid = retval; 
+						wait(&pid);
+					}
+					else{
+
+					}
+				}
+			}
+			
+			indexPointer = newIndex;
+		}
 	}
-}			
+	else if(isBackgroundCommand(command)){
+		background = true;
+	    int retval = fork()
+	    if(retval == 0)
+		    executeShellBuiltin(command);
+	    else{
+	        
+	    }
+	}
+	else{
+	    int retval = fork();
+	    if(retval == 0)
+		    executeShellBuiltin(command);
+	    else{
+			int status;
+			int pid = retval;
+			if(!background || !lastParallel)
+				waitpid(pid, &status, 0);
+		}
+	}	
+}		
                 
                 
                 
